4eea068e36368f03110773adb9132c6fc3e8d1f1
jobweeda <jobweeda@hotmail.com>
2017-09-09 13:17:22 +0200

    add names to artists and songs in DB

---
+++ db/migrate/20170909110942_add_song_names_to_songs.rb
@@ -0,0 +1,5 @@
+class AddSongNamesToSongs < ActiveRecord::Migration[5.1]
+  def change
+    add_column :songs, :song_name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909104620) do
+ActiveRecord::Schema.define(version: 20170909111633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,16 @@ ActiveRecord::Schema.define(version: 20170909104620) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "artist_name"
   end
 
   create_table "songs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.string "song_name"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end
