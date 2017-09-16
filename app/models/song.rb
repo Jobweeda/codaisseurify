b807fce06348d71220400213566b2b00569c4dd0
jobweeda <jobweeda@hotmail.com>
2017-09-16 10:59:56 +0200

    updates songmodel

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
-    belongs_to :artist
+    belongs_to :artist, optional: true
 
     validates :name, presence: true
-    validates :name, uniqueness: true 
+    validates :name, uniqueness: true
 end
