a3c6b86e91c52e166320db76e2aea3c34e328f0d
jobweeda <jobweeda@hotmail.com>
2017-09-16 13:14:45 +0200

    adds form into showpage

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -1,18 +1,39 @@
 function submitSong(event) {
   event.preventDefault();
+
+
+
   createSong($("#song_name").val());
   $("#song_name").val(null);
 }
 
+function createSong(title) {
+  var checkboxId = "song-" + nextSongId();
+
+  var label = $('<label></label>')
+    .attr('for', checkboxId)
+    .html(title);
+
+  var checkbox = $('<input type="checkbox" value="1" />')
+    .attr('id', checkboxId)
+    .bind('change', toggleDone);
+
+  var tableRow = $('<tr class="todo"></td>')
+    .append($('<td>').append(checkbox))
+    .append($('<td>').append(label));
+
+  $(".all-songs").append(songItem);
+
+
 function destroySongs(event) {
   event.preventDefault();
   $.when($(".success").remove())
-  .then($())
+
 
 }
 
 $(document).ready(function() {
   $("input[type=checkbox]").bind('change', toggleDone);
   $("form").bind('submit', submitSong);
-  $("#clean-up").bind('click', destroySongs);
+  $("#remove-songs").bind('click', destroySongs);
 });
