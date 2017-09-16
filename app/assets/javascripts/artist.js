a589f44baac0d0fa9ab5ddfc66d3d01b93b6a9f1
jobweeda <jobweeda@hotmail.com>
2017-09-16 15:55:59 +0200

    adds the documentready function

--- app/assets/javascripts/artist.js
+++ app/assets/javascripts/artist.js
@@ -4,5 +4,34 @@ function submitSong(event) {
 
   createSong(songName);
   $('#new-song').val(null);
-  
+
 }
+function createSong(songName) {
+  var checkboxId = "song-" + nextSongId();
+
+  var listItem = $("<li></li>");
+  listItem.addClass("song");
+
+
+
+  var space = document.createTextNode(" ");
+
+
+
+
+  listItem.append(space);
+
+
+  $("#song").append( listItem );
+
+
+}
+
+function nextSongId() {
+  return $(".song").length + 1;
+}
+
+
+$(document).ready(function() {
+$("form").bind('submit', submitSong);
+});
